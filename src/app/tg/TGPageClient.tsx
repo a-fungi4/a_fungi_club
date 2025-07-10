c25f65eb5cf82e8a00d1e96bc882c43cef113c75
a-fungi4 <khaled@b8momani.com>
2025-07-10 03:46:30 -0500

    chore: commit user local changes

--- src/app/tg/TGPageClient.tsx
+++ src/app/tg/TGPageClient.tsx
@@ -173,7 +173,6 @@ export default function TGPageClient() {
       <Banner title="Tacky Garbage Collection" variant="general" className="fullBleed">
         <div className="Bannerprojectembed1">
           <div className="BannerTextBox">
-            <p>This is the TG page content. Add your information here.</p>
           </div>
         </div>
       </Banner>
